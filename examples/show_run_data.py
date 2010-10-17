b8d759d321d392bc75b629867934a0372a59cf24
Michael Goerz <goerz@physik.fu-berlin.de>
2010-10-17 22:32:58 +0200

    Improved output of show_run_data script
    Print less decimal places. Print min:sec instead of decimal minutes

--- examples/show_run_data.py
+++ examples/show_run_data.py
@@ -8,6 +8,10 @@ from RunkeeperAnalyze.RunData import Run
 
 filename = sys.argv[-1]
 
+def m2t(dec_min):
+    """ Convert decimal minutes to min:sec """
+    return "%02i:%02i" % (dec_min, (dec_min - int(dec_min)) * 60)
+
 r = Run(filename)
 r.segmentize()
 accumulated_distance = 0
@@ -34,26 +38,27 @@ for i, segment in enumerate(r.segments):
 
 for i, segment in enumerate(r.segments):
     print "\n** Segment %i Summary **" % (i + 1)
-    print "Time (min)              : %s" % segment.total_time(unit='min')
-    print "Distance (km, miles)    : %s, %s" % (
+    print "Time (min)              : %s" % m2t(segment.total_time(unit='min'))
+    print "Distance (km, miles)    : %.3f, %.3f" % (
         segment.total_distance(unit='km'), segment.total_distance(unit='miles'))
-    print "Speed (km/h, mph)       : %s, %s" % (
+    print "Speed (km/h, mph)       : %.3f, %.3f" % (
         segment.average_speed(dunit='km'), segment.average_speed(dunit='miles'))
     print "Pace (min/km, min/mile) : %s, %s" % (
-        segment.average_pace(dunit='km'), segment.average_pace(dunit='mile'))
+        m2t(segment.average_pace(dunit='km')),
+        m2t(segment.average_pace(dunit='mile')))
 
 
 print "\n**** Run Summary ****\n"
-print "Total time (min)          : %s" % r.total_time(unit='min')
-print "Active time (min)         : %s" % r.active_time(unit='min')
-print "Tot. distance (km, miles) : %s, %s" % (r.total_distance(unit='km'),
-                                              r.total_distance(unit='miles'))
-print "Act. distance (km, miles) : %s, %s" % (r.active_distance(unit='km'),
-                                              r.active_distance(unit='miles'))
-print "Skip distance (km, miles) : %s, %s" % (r.skipped_distance(unit='km'),
-                                              r.skipped_distance(unit='miles'))
+print "Total time (min)          : %s" % m2t(r.total_time(unit='min'))
+print "Active time (min)         : %s" % m2t(r.active_time(unit='min'))
+print "Tot. distance (km, miles) : %.3f, %.3f" % (
+              r.total_distance(unit='km'), r.total_distance(unit='miles'))
+print "Act. distance (km, miles) : %.3f, %.3f" % (
+              r.active_distance(unit='km'), r.active_distance(unit='miles'))
+print "Skip distance (km, miles) : %.3f, %.3f" % (
+              r.skipped_distance(unit='km'), r.skipped_distance(unit='miles'))
 print "Segments                  : %s" % len(r.segments)
-print "Speed (km/h, mph)         : %s, %s" % (
-                    r.average_speed(dunit='km'), r.average_speed(dunit='mile'))
+print "Speed (km/h, mph)         : %.3f, %.3f" % (
+             r.average_speed(dunit='km'), r.average_speed(dunit='mile'))
 print "Pace (min/km, min/mile)   : %s, %s" % (
-                    r.average_pace(dunit='km'), r.average_pace(dunit='mile'))
+             m2t(r.average_pace(dunit='km')), m2t(r.average_pace(dunit='mile')))
