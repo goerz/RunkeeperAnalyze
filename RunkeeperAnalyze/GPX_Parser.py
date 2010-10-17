dccb5c9e9ac1aee2aed654acfb635b84f972de28
Michael Goerz <goerz@physik.fu-berlin.de>
2010-10-17 14:06:33 +0200

    Calculate distance, speed, pace between trackpoints

--- RunkeeperAnalyze/GPX_Parser.py
+++ RunkeeperAnalyze/GPX_Parser.py
@@ -19,10 +19,21 @@
 #    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
 ############################################################################
 
-""" Parser for Runkeeper GPX data """
+""" Parser for getting Trackpoints from Runkeeper GPX data """
 
-from xml.dom import minidom    
+from xml.dom import minidom
 import time
+from math import pi, sin, cos, tan, atan, sqrt, asin, atan2
+
+# http://www.movable-type.co.uk/scripts/latlong-vincenty.html
+EARTH_MAJOR_AXIS = 6378388
+EARTH_MINOR_AXIS = 6356911.946
+
+DISTANCE = {'m' : 1, 'km' : 1000, 'miles' : 1609.344, 'mile': 1609.344}
+TIMES = {'min':60, 'hour': 3600, 'day' : 86400}
+
+PREF_DUNIT = 'km'
+
 
 class Trackpoint:
     """ Class representing a single track point """
@@ -32,11 +43,112 @@ class Trackpoint:
         self.timestamp = timestamp
         self.elevation = elevation
     def __str__(self):
-        return "(%s,%s) elev %s at %s" % (self.latitude, self.longitude, 
-                                          self.elevation, self.timestamp)
-    def distance_to(self, trackpoint):
-        """ Calculate the distance (in m) to another trackpoint """
-        pass
+        return "(%s,%s) elev %s on %s" % (self.latitude, self.longitude,
+                                          self.elevation, self.time_str())
+    def time_tuple(self):
+        """ Return time tuple for the Trackpoint's timestamp """
+        return time.localtime(self.timestamp)
+    def time_str(self):
+        """ Return string for the Trackpoint's timestamp """
+        return time.ctime(self.timestamp)
+    def time_to(self, trackpoint):
+        """ Calculated the time (in seconds) between this trackpoint and
+            another one
+        """
+        return abs(self.timestamp - trackpoint.timestamp)
+    def speed_to(self, trackpoint, dunit=PREF_DUNIT, tunit='hour', pause=0):
+        """ Calculate speed between this trackpoint and another, in
+            dunit/tunit. The time period used for the calculation is the
+            difference between the timestamps, reduced by pause (in seconds)
+        """
+        time_diff = self.time_to(trackpoint) - pause
+        dist_diff = self.distance_to(trackpoint)
+        try:
+            return (TIMES[tunit] * dist_diff ) / (DISTANCE[dunit] * time_diff)
+        except ZeroDivisionError:
+            return 0.0
+    def pace_to(self, trackpoint, dunit=PREF_DUNIT, tunit='min', pause=0):
+        """ Calculate pace between this trackpoint and another, in
+            tunit/dunit. The time period used for the calculation is the
+            difference between the timestamps, reduced by pause (in seconds)
+        """
+        time_diff = self.time_to(trackpoint) - pause
+        dist_diff = self.distance_to(trackpoint)
+        try:
+            return (DISTANCE[dunit] * time_diff) / (TIMES[tunit] * dist_diff )
+        except ZeroDivisionError:
+            return 0.0
+    def distance_to(self, trackpoint, use_elevation=True, unit='m'):
+        """ Calculate the distance to another trackpoint """
+        # adapted from
+        # http://www.mathworks.com/matlabcentral/fileexchange/5379
+        a = EARTH_MAJOR_AXIS
+        b = EARTH_MINOR_AXIS
+        # convert coordinates to radians
+        lat1 = self.latitude * 0.0174532925199433
+        lon1 = self.longitude * 0.0174532925199433
+        lat2 = trackpoint.latitude * 0.0174532925199433
+        lon2 = trackpoint.longitude * 0.0174532925199433
+        sign = lambda a: int(a>0) - int(a<0)
+        # correct for errors at exact poles by adjusting 0.6 millimeters:
+        if abs( pi / 2 - abs(lat1) ) < 1e-10:
+            lat1 = sign(lat1) * ( pi / 2.0 - 1e-10 )
+        if abs( pi / 2.0 - abs(lat2) ) < 1e-10:
+            lat2 = sign(lat2) * ( pi / 2.0 -1e-10 )
+        f = (a-b)/a
+        U1 = atan((1-f) * tan(lat1))
+        U2 = atan((1-f) * tan(lat2))
+        lon1 = lon1 % (2 * pi)
+        lon2 = lon2 % (2 * pi)
+        L = abs(lon2-lon1)
+        if L > pi:
+            L = 2*pi - L
+        lambda_v = L
+        lambdaold = 0
+        itercount = 0
+        while True:
+            itercount = itercount + 1
+            if itercount > 50:
+                # Points are essentially antipodal. Precision may be reduced
+                # slightly.
+                lambda_v = pi
+                break
+            lambdaold = lambda_v
+            sinsigma = sqrt(   (cos(U2)*sin(lambda_v))**2
+                             + (cos(U1)*sin(U2)
+                                - sin(U1)*cos(U2)*cos(lambda_v))**2 )
+            cossigma = sin(U1)*sin(U2) + cos(U1)*cos(U2)*cos(lambda_v)
+            sigma = atan2(sinsigma, cossigma)
+            try:
+                alpha = asin( cos(U1)*cos(U2)*sin(lambda_v) / sin(sigma) )
+            except ZeroDivisionError:
+                alpha = 0.0
+            cos2sigmam = cos(sigma) - 2*sin(U1)*sin(U2) / cos(alpha)**2
+            C = f/16 * cos(alpha)**2 * (4 + f * (4-3*cos(alpha)**2))
+            lambda_v = L + (1-C) * f * sin(alpha) * ( sigma+C*sin(sigma) \
+                       * (cos2sigmam+C*cos(sigma)*(-1+2*cos2sigmam**2)) )
+            # correct for convergence failure in the case of essentially
+            # antipodal points
+            if lambda_v > pi:
+                # Points are essentially antipodal. Precision may be reduced
+                # slightly.
+                lambda_v = pi
+                break
+            if abs(lambda_v-lambdaold) > 1e-12:
+                break
+        u2 = cos(alpha)**2 * (a**2-b**2) / b**2
+        A = 1 + u2/16384 * ( 4096 + u2*(-768 + u2*(320-175*u2)) )
+        B = u2 / 1024 * ( 256 + u2*(-128 + u2*(74-47*u2)) )
+        deltasigma = B * sin(sigma) * ( \
+                     cos2sigmam+B/4*(cos(sigma) *(-1+2*cos2sigmam**2) \
+                     - B/6 * cos2sigmam * (-3 + 4*sin(sigma)**2) \
+                       * (-3+4*cos2sigmam**2))  )
+        if use_elevation:
+            height_diff = self.elevation - trackpoint.elevation
+            return sqrt( (b * A * (sigma-deltasigma))**2
+                         + height_diff**2 ) / DISTANCE[unit]
+        else:
+            return b * A * (sigma-deltasigma) / DISTANCE[unit]
 
 
 class GPX_Error(ValueError):
@@ -67,7 +179,7 @@ class GPX_Parser:
         return self
     def number_of_segments(self):
         """ Return number of segments that are still available to be iterated
-            over 
+            over
         """
         curnode = self._curnode
         while curnode.nodeName != 'trkseg':
@@ -115,8 +227,8 @@ class GPX_Parser:
         """ Return a Trackpoint instance generated from self._curnode, which
             must be a trkpt node
         """
-        result = Trackpoint(latitude=self._curnode.getAttribute('lat'),
-                            longitude=self._curnode.getAttribute('lon'))
+        result = Trackpoint(latitude=float(self._curnode.getAttribute('lat')),
+                            longitude=float(self._curnode.getAttribute('lon')))
         for trkpt_data in self._curnode.childNodes:
             if trkpt_data.nodeName == 'ele':
                 elevation = ''
@@ -130,6 +242,6 @@ class GPX_Parser:
                     if text_data.nodeType == text_data.TEXT_NODE:
                         time_string = time_string + text_data.nodeValue
                 time_string = time_string.strip()
-                result.timestamp = time.mktime(time.strptime(time_string, 
+                result.timestamp = time.mktime(time.strptime(time_string,
                                    "%Y-%m-%dT%H:%M:%SZ"))
         return result
